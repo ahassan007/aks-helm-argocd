1f0f40f855760143c0cdbe4102e535ede61c4ceb
Abdikadir <abdikadir013@icloud.com>
2025-05-04 12:55:27 +0100

    Fixing data ref

--- data.tf
+++ data.tf
@@ -3,11 +3,10 @@ data "azurerm_user_assigned_identity" "this" {
     for key, value in var.data_look_prerequisites : key => value if key == "managed_identity"
   }
   name                = each.value.name
-  location            = each.value.location
   resource_group_name = each.value.resource_group_name
   
 }
 
 output "mi" {
-  value = azurerm_user_assigned_identity.this
+  value = data.azurerm_user_assigned_identity.this
 }
